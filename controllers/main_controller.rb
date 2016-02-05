0e8b05cf28be0bfe6b787e234a76b1fabcd8ef60
Kyle Sandage <ksandage08@gmail.com>
2016-02-04 18:44:52 -0600

    Kyle

--- controllers/main_controller.rb
+++ controllers/main_controller.rb
@@ -9,3 +9,23 @@ MyApp.get "/" do
   # combined document back to the client.
   erb :"main/welcome"
 end
+
+MyApp.get "/about" do
+
+  erb :"main/about"
+end
+
+MyApp.get "/favorites" do
+
+  erb :"main/favorites"
+end
+
+MyApp.get "/greet/sumeet" do
+
+  erb :"main/greet/sumeet"
+end
+
+MyApp.get "/greet/:num" do
+
+  erb :"main/greet/person"
+end
